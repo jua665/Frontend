f499341bb239eede396094c9fceff07609145890
jua665 <117867140+jua665@users.noreply.github.com>
2025-03-07 08:04:48 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Asegúrate de exponer tu aplicación en todas las interfaces
-    port: 10000,      // El puerto que Render espera
+    allowedHosts: ['frontend-f9lt.onrender.com'], // Agrega tu dominio aquí
   },
-})
+});
