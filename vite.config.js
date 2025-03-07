6a71e0e6512eceb45ead2a0d05e7d4d3e167ff88
jua665 <117867140+jua665@users.noreply.github.com>
2025-03-06 23:09:03 -0600

    a

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Permite que la aplicación sea accesible desde cualquier interfaz de red
-    port: 5000,      // El puerto que Render espera
-  }
 })
